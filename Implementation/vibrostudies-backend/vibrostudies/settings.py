4f0b815d14e0bf87ba599ca6b7e198611bb58eb6
RDNRofficial <minecraft.lorrdnob@gmail.com>
2020-10-29 12:45:21 +0100

    Update settings.py

--- Implementation/vibrostudies-backend/vibrostudies/settings.py
+++ Implementation/vibrostudies-backend/vibrostudies/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o(#eb27l-!#u^jgh!5t4kpubzwhcc4u+o+f2h4qm--_2+j-nf2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
